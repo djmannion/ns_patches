7690bc2c339d1c01f1eebd6089c89a7298fcd0ec
Damien Mannion <d.mannion@unsw.edu.au>
2013-06-12 11:19:28 -0500

    -

--- ns_patches_demo.py
+++ ns_patches_demo.py
@@ -1,4 +1,5 @@
 
+import os.path
 import collections
 import random
 
@@ -27,13 +28,13 @@ def main():
 	                           )
 
 	ring_thetas = [ [ 0, 60, 120, 180, 240, 300 ],
-	                [ 0, 30, 60, 120, 150, 180, 210, 240, 300, 330 ],
+	                [ 0, 35, 70, 110, 145, 180, 215, 250, 290, 325 ],
 	                [ 0, 30, 60, 120, 150, 180, 210, 240, 300, 330 ],
 	              ]
 
-	ring_ecc = [ 53, 106, 177 ]
+	ring_ecc = [ 62, 123, 212 ]
 
-	ring_diam = [ 56, 72, 94 ]
+	ring_diam = [ 89, 117, 158 ]
 
 	mask_info = []
 
@@ -69,8 +70,89 @@ def main():
 
 	masks = make_masks( mask_info )
 
-	return masks
+	n_masks = len( masks )
 
+	n_coh = 13
+
+	win = psychopy.visual.Window( ( 512, 512 ),
+	                              fullscr = False,
+	                              allowGUI = True,
+	                              units = "pix"
+	                            )
+
+	try:
+		stims = init_stims( win, masks )
+
+		keep_going = True
+
+		while keep_going:
+
+			set_stims( win, n_coh, img_list, stims )
+
+			_ = [ stim.draw() for stim in stims ]
+
+			win.flip()
+
+			keys = psychopy.event.waitKeys()
+
+			if "q" in keys:
+				keep_going = False
+			elif "s" in keys:
+				win.getMovieFrame()
+				win.saveMovieFrames( "cap.png" )
+
+
+	except:
+		win.close()
+		raise
+
+	win.close()
+
+
+def set_stims( win, n_coh, img_list, stims ):
+
+	coh_img = random.choice( img_list )
+
+	other_img = [ random.choice( img_list ) for _ in stims ]
+
+	img = [ coh_img ] * n_coh + other_img[ :( len( stims ) - n_coh ) ]
+	random.shuffle( img )
+
+	for ( i_stim, stim ) in enumerate( stims ):
+
+		print img[ i_stim ]
+
+		img_dir, img_name = os.path.split( img[ i_stim ] )
+
+		img_path = os.path.join( "/home/damien/mcgill_db",
+		                         img_dir, img_name
+		                       )
+
+		img_rast = stimuli.mcgill_db.read_image( img_path, True, True, True )
+
+		stim.setImage( np.flipud( img_rast[ :512, :512 ] ) )
+
+	return stims
+
+def init_stims( win, masks ):
+
+	stim = collections.namedtuple( "stim", [ "tex", "img", "i_mask", "type" ] )
+
+	stims = []
+
+	n_stim = len( masks )
+
+	for mask in masks:
+
+		stims.append( psychopy.visual.ImageStim( win = win,
+		                                         image = np.ones( ( 512, 512 ) ),
+		                                         mask = mask,
+		                                         units = "pix",
+		                                         size = ( 512, 512 ),
+		                                       )
+		            )
+
+	return stims
 
 
 def make_masks( mask_info ):
