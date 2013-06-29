f8fbde527d8e16f949e196b04f00229283ef1361
Damien Mannion <d.mannion@unsw.edu.au>
2013-06-29 15:58:49 -0500

    -

--- stimulus.py
+++ stimulus.py
@@ -5,77 +5,6 @@ import psychopy.visual, psychopy.filters
 import stimuli.utils, stimuli.mcgill_db
 
 
-class Stimulus( object ):
-	"""NS patches experiment stimulus object.
-
-	Parameters
-	----------
-	win : PsychoPy window instance
-		Window to draw to.
-	img : string or ndarray
-		Either a path to an image, or the image itself.
-	mask : ndarray, optional
-		Mask to apply to the image.
-
-	"""
-
-	def __init__( self,
-	              win,
-	              img,
-	              mask = None
-	            ):
-
-		self._win = win
-		self._img = img
-		self._mask = mask
-
-		self._gen_tex()
-
-		self.update_image( self._img )
-
-		self.update_mask( self._mask )
-
-
-	def draw( self ):
-		"""Draws the stimulus to the window"""
-
-		self._img_tex.draw()
-
-
-	def _gen_tex( self ):
-		"""Generates a texture image"""
-
-		self._img_tex = psychopy.visual.ImageStim( win = self._win,
-		                                           image = np.ones( ( 256, 256 ) ),
-		                                           size = ( 256, 256 ),
-		                                           units = "pix"
-		                                         )
-
-	def update_image( self, img ):
-
-		self._img = img
-
-		if type( self._img ) == np.ndarray:
-			image = self._img
-		else:
-			image = load_img( self._img )
-
-		self._img_size = image.shape
-
-		self._img_tex.setImage( image )
-		self._img_tex.setSize( self._img_size )
-
-
-	def update_mask( self, mask ):
-
-		self._mask = mask
-
-		if self._mask is None:
-			self._mask = np.ones( self._img_size )
-
-		self._img_tex.setMask( mask )
-
-
 def load_img( img_path ):
 
 	image = stimuli.mcgill_db.read_image( img_path = img_path,
@@ -94,7 +23,8 @@ def load_img( img_path ):
 def make_mask( patch,
                img_size,
                mask_shape = "raisedCosine",
-               mask_fringe = 0.4
+               mask_fringe = 0.4,
+               mask_range = ( -1, +1 )
              ):
 
 	mask_func = psychopy.filters.makeMask
@@ -109,13 +39,71 @@ def make_mask( patch,
 	                  shape = mask_shape,
 	                  radius = [ norm_rad ] * 2,
 	                  center = norm_xy,
-	                  fringeWidth = mask_fringe
+	                  fringeWidth = mask_fringe,
+	                  range = mask_range
 	                )
 
 	return mask
 
 
+def get_fixation( win, conf ):
 
+	fix_stim = []
 
+	h_frac = 0.625
 
+	l_s = [ ( +1, +0 ), ( +h_frac, +1 ), ( +0, +1 ), ( -h_frac, +1 ) ]
+	l_e = [ ( -1, +0 ), ( -h_frac, -1 ), ( +0, -1 ), ( +h_frac, -1 ) ]
 
+	grid = [ psychopy.visual.Line( win,
+	                               start = ls,
+	                               end = le,
+	                               units = "norm",
+	                               lineColor = [ -0.25 ] * 3,
+	                               lineWidth = 1.5
+	                             )
+	         for ( ls, le ) in zip( l_s, l_e )
+	       ]
+
+	fix_stim.extend( grid )
+
+	grids_r_va = [ 0.5, 1.8, 3.5, 6.1, 8.5 ]
+
+	grid_lum = -0.25
+
+	# list of circle stimuli
+	rings = [ psychopy.visual.Circle( win,
+	                                  radius = grid_r_va,
+	                                  units = "deg",
+	                                  edges = 96,
+	                                  lineColor = grid_lum,
+	                                  lineWidth = 1.5
+	                                )
+	          for grid_r_va in grids_r_va
+	        ]
+
+	fix_stim.extend( rings )
+
+	outlines = [ psychopy.visual.Circle( win = win,
+	                                     pos = ( patch[ "cx" ], patch[ "cy" ] ),
+	                                     units = "pix",
+	                                     radius = patch[ "diam" ] / 2.0,
+	                                     lineColor = [ -0.25 ] * 3,
+	                                     fillColor = [ 0 ] * 3,
+	                                     lineWidth = 1.5,
+	                                   )
+	             for patch in conf.stim.patches
+	           ]
+
+	fix_stim.extend( outlines )
+
+	fixation = psychopy.visual.Circle( win = win,
+	                                   radius = 4,
+	                                   units = "pix",
+	                                   fillColor = [ -1, 1, -1 ],
+	                                   lineWidth = 1
+	                                 )
+
+	fix_stim.append( fixation )
+
+	return fix_stim
